d2417120bec42d3d9a4f2e74e0d3741e07c38534
DesmondFur <desfurtick@gmail.com>
2019-07-03 16:06:30 -0600

    PAHNINI

---
+++ vanilla-piano/script.js
@@ -0,0 +1,16 @@
+let el = document.querySelector('audio[data-key="97"]')
+
+window.addEventListener("keypress", function(keyPressEvent) {
+    let tone = document.querySelector(`audio[data-key="${keyPressEvent.keyCode}"]`);
+    if (!tone) return;
+    let pianoKey = document.querySelector(`.pianoKey[data-key="${keyPressEvent.keyCode}"]`);
+    tone.currentTime = 0
+    pianoKey.classList.add('pressed')
+    tone.play()
+    setTimeout( function(){
+        pianoKey.classList.remove('pressed')
+    }, 300    )
+
+});
+
+
